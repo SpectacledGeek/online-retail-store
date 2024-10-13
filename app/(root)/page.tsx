1cd03258e4a4b2bf7c761ed4895fb88a26b5dcb8
Suryansh Singh <suryanshsingh1807@gmail.com>
2024-10-13 22:07:32 +0530

    CITL Project Initlaialization

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4 h-screen flex justify-center items-center">
+      <Button size="lg">Touch Me</Button>
+    </div>
+  );
+}
